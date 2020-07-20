59b5a778d79069f879448a31ed622ba4b4edd54f
esilean <le.bevilaqua@gmail.com>
2020-07-20 12:01:07 -0300

    added order event

--- src/events/subjects/subjects.ts
+++ src/events/subjects/subjects.ts
@@ -2,4 +2,7 @@ export enum Subjects {
   TicketCreated = "ticket:created",
   TicketUpdated = "ticket:updated",
   TicketDeleted = "ticket:deleted",
+
+  OrderCreated = "order:created",
+  OrderCancelled = "order:cancelled",
 }
--- src/index.ts
+++ src/index.ts
@@ -17,4 +17,7 @@ export * from "./events/tickets/ticket-created-event";
 export * from "./events/tickets/ticket-deleted-event";
 export * from "./events/tickets/ticket-updated-event";
 
+export * from "./events/orders/order-created-event";
+export * from "./events/orders/order-cancelled-event";
+
 export * from "./events/types/order-status";
